c74ac9608451da880d14b44880b912d00ef2b411
evandri30 <evandriridhoh@gmail.com>
2026-08-23 15:02:29 +0700

    changes roles

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -1,1 +1,1 @@
-export const roles = ["Software Engineer", "Typescript", 'Python', 'PHP'];
+export const roles = ["Software Engineering & Data Analysis", "Python, TypeScript, SQL", "Ex-IT Intern @ Pelita Air Service"];
